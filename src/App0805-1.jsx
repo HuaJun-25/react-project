f1a3940c99341c1d65677b22006acc6c1d7b491f
HuaJun-25 <izuankwii@gmail.com>
2025-08-05 16:51:13 +0800

    class

---
+++ src/App0805-1.jsx
@@ -0,0 +1,25 @@
+import './css/app0805-1.css'
+
+function App() {
+    // 一般變數
+    const pokemon ="poketa"
+    // 類別/屬性名稱變數
+    const myClassName="myClass"
+    // 方法
+    function myClick(){
+        alert('我是按鈕2')
+    }
+    return (
+        // return只能單個element
+        // <></>這種寫法稱之為Fragment的虛擬元素，不會被渲染出來，也就不會多一層無意義的div */
+        <>
+            <h1>Hello</h1>
+            <div>
+                <p style={{color:"red"}} className={myClassName}>姓名:{pokemon}</p>
+                <button type="button" onClick={()=>{alert('我是按鈕1')}}>按鈕1</button>
+                <button type="button" onClick={myClick}>按鈕2</button>
+            </div>
+        </>
+    )
+}
+export default App;
